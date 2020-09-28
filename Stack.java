c185b3355505bf438e9120f25ec53648ee2d4f6e
Leekwonhyo <kensi0512@gmail.com>
2020-09-28 12:52:05 +0900

    this is stack interface

---
+++ Stack.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of interface Stack here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface Stack<T>
+{
+   public abstract T pop();
+   public abstract boolean push(T obj);
+}
